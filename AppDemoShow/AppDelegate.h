4cddb11b6eda9dcdeef45df47f913358dc81a17e
ethan.lxb <ethan.lxb@alibaba-inc.com>
2015-06-01 16:24:09 +0800

    Initial Commit

---
+++ AppDemoShow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AppDemoShow
+//
+//  Created by linxiaobin on 15/6/1.
+//  Copyright (c) 2015年 linxiaobin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AppDemoShow/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AppDemoShow
+//
+//  Created by linxiaobin on 15/6/1.
+//  Copyright (c) 2015年 linxiaobin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
